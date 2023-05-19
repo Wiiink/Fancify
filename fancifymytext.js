c4f730250bbec350220fa07facea8961be8f26e9
Luis Lujan <90700993+Wiiink@users.noreply.github.com>
2023-05-19 06:59:06 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,49 @@
+const text = document.getElementById("text");
+const big = document.querySelector("#bigger");
+const fancy = document.getElementById("fancy");
+const boring = document.getElementById("boring");
+const defColor = document.getElementById("text").style.color;
+const moo = document.getElementById("moo");
+
+
+const bigger = () => {
+  alert("Hello, world!");
+  text.style.fontSize = "24pt";
+};
+
+
+big.onclick = () => {
+  bigger();
+};
+
+
+const fancify = () => {
+  alert("fancified!");
+  text.style.fontWeight = "bold";
+  text.style.color = "blue";
+  text.style.textDecoration = "underline";
+};
+
+
+fancy.onchange = (event) => {
+  if (event.target.checked) {
+    fancify();
+  }
+};
+
+
+boring.onchange = (event) => {
+  if (event.target.checked) {
+    text.style.fontWeight = "normal";
+    text.style.color = "black";
+    text.style.textDecoration = "none";
+  }
+};
+
+
+moo.onclick = () => {
+  alert("moo");
+  var newText = text.value.toUpperCase();
+  var sentences = newText.split(".");
+  text.value = sentences.join("-Moo. ");
+};
